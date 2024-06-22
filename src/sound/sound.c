49525fcd5c6c46b99509ec04b791e700b396f765
Josep Maria Antolín Segura <josepma@gmail.com>
2024-06-22 03:19:39 +0200

    Fix sound config button. Fixes Issue #235 (#265)

--- src/sound/sound.c
+++ src/sound/sound.c
@@ -73,7 +73,7 @@ device_t *sound_card_getdevice(int card) {
 }
 
 int sound_card_has_config(int card) {
-        if (sound_cards[card] != NULL || !sound_cards[card]->device)
+        if (sound_cards[card] == NULL || sound_cards[card]->device == NULL)
                 return 0;
         return sound_cards[card]->device->config ? 1 : 0;
 }
